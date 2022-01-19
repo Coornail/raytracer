c07b211348700f1efdc4321f4b2d1887f16cb753
Kornel Lugosi <coornail@gmail.com>
2022-01-19 18:34:10 +0100

    Clean up sphere.go

--- sphere.go
+++ sphere.go
@@ -2,27 +2,13 @@ package main
 
 import "math"
 
-func HitSphere(center Vec3, radius float64, r Ray) float64 {
-	oc := r.Origin.Sub(center)
-	a := dot(r.Direction, r.Direction)
-	b := 2.0 * dot(oc, r.Direction)
-	c := dot(oc, oc) - radius*radius
-	discriminant := b*b - 4*a*c
-	if discriminant < 0 {
-		return -1.0
-	}
-
-	return (-b - math.Sqrt(discriminant)) / (2.0 * a)
-}
-
 func dot(v1, v2 Vec3) float64 {
 	return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
 }
 
-
 type Sphere struct {
-	Center Vec3
-	Radius float64
+	Center   Vec3
+	Radius   float64
 	Material Material
 }
 
